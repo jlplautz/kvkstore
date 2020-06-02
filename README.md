361184d1082e74989cb7266f2c948c6d0faaaa56
61246880 <jorge.plautz@gmail.com>
2020-06-02 18:10:01 -0300

    Verificado conectividade com heroku
    close #14

--- README.md
+++ README.md
@@ -100,3 +100,4 @@ SECRET_KEY: secreta-key gerada
 ```
 
 
+
--- store/settings.py
+++ store/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = True
 DEBUG = config('DEBUG', cast=bool)
 
+# ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
