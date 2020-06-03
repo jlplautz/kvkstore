588a98c34a9f92da4ec3bdf528973ee32213513c
61246880 <jorge.plautz@gmail.com>
2020-06-03 00:30:38 -0300

    Corrigido file settings.py - collectfast
    close #33

--- store/settings.py
+++ store/settings.py
@@ -37,6 +37,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -44,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
     # Apps de terceiros
     'widget_tweaks',
