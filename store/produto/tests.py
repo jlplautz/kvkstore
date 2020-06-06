712a89021a2e613cf3f717ee83d8b2e1d73df3de
61246880 <jorge.plautz@gmail.com>
2020-06-06 09:20:03 -0300

    Reinstalado a lib Pillow e ativado testes
    close #57

--- store/produto/tests.py
+++ store/produto/tests.py
@@ -1,5 +1,5 @@
 from store.produto.apps import ProdutoConfig
 
 
-def test_produto():
+def test_test_app_config_name():
     assert ProdutoConfig.name == 'produto'
--- store/users/tests.py
+++ store/users/tests.py
@@ -1,5 +1,5 @@
 from store.users.apps import UsersConfig
 
 
-def test_users():
+def test_test_app_config_name():
     assert UsersConfig.name == 'users'
