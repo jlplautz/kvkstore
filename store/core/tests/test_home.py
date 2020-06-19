08be6cd8824d9efe077972883bb305cbc69284be
61246880 <jorge.plautz@gmail.com>
2020-06-19 17:52:19 -0300

    kvkstore-95 Modificar estrutura de testes

--- store/core/tests/test_home.py
+++ store/core/tests/test_home.py
@@ -1,16 +1,16 @@
-from django.test import Client
+# from django.test import Client
 from store.core.apps import CoreConfig
 
 
-def test_status_code(client: Client):
-    """quando accessamos nosso site fazemos uma chamada do tipo get
-    logo na raiz da aplicação. Justamente a view que construimos
-
-    Emulando um request o protocolo http vai retornar um response.
-    Aqui queremos nos certificar que esta resposta retornou com sucesso
-    """
-    resp = client.get('/')
-    assert resp.status_code == 200
+# def test_status_code(client: Client):
+#     """quando accessamos nosso site fazemos uma chamada do tipo get
+#     logo na raiz da aplicação. Justamente a view que construimos
+#
+#     Emulando um request o protocolo http vai retornar um response.
+#     Aqui queremos nos certificar que esta resposta retornou com sucesso
+#     """
+#     resp = client.get('/')
+#     assert resp.status_code == 200
 
 
 def test_core():
