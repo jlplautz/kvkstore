6d05b7d41d051d7624462d73a138efdbe2dd2ed3
61246880 <jorge.plautz@gmail.com>
2020-06-19 10:53:47 -0300

    up branch for dependencies update

--- store/settings.py
+++ store/settings.py
@@ -28,6 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = '6lp%j9$@4*_sjakb+ek%r5eary!v16d$rdy)fv4^71xdycur9$'
 SECRET_KEY = config('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = config('DEBUG', default=False, cast=bool)
